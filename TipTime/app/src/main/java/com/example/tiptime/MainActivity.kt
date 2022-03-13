9375d7f749681ee09770fc24d3187f3210f310c0
mtnmr <mei.rila1013@gmail.com>
2022-03-13 17:23:43 +0900

    keyboard hide function

--- TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.tiptime
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.example.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
 import java.util.*
@@ -17,8 +21,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
         binding.calculateButton.setOnClickListener{ calculateTip()}
+
+        binding.costOfServiceEdit.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode) }
     }
 
     private fun calculateTip() {
@@ -49,6 +54,17 @@ class MainActivity : AppCompatActivity() {
     private fun displayTip(tip : Double) {
         val formattedTip = NumberFormat.getCurrencyInstance(Locale.US).format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+
+    private fun handleKeyEvent(view: View, keyCode:Int):Boolean{
+        if (keyCode == KeyEvent.KEYCODE_ENTER){
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+            return true
+        }
+
+        return false
     }
 
 }
