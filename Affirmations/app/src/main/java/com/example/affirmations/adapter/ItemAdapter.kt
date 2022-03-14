012d6187ada69bf4d94c8a826a41a3607db48369
mtnmr <mei.rila1013@gmail.com>
2022-03-14 14:47:19 +0900

    UI

--- Affirmations/app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
+++ Affirmations/app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -12,12 +12,12 @@ import com.example.affirmations.model.Affirmation
 
 class ItemAdapter(
     private val context: Context,
-    private val dataset:List<Affirmation>
-    ):RecyclerView.Adapter<ItemAdapter.ItemViewHolder>(){
+    private val dataset: List<Affirmation>
+) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
-    class ItemViewHolder(private val view:View):RecyclerView.ViewHolder(view){
-        val textView:TextView = view.findViewById(R.id.item_title)
-        val imageView:ImageView = view.findViewById(R.id.item_image)
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
 
