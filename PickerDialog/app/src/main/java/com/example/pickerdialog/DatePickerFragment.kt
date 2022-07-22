2a483c6118233cac220698f8695ca853ef82d0dd
mtnmr <mei.rila1013@gmail.com>
2022-07-22 11:18:00 +0900

    TimePickerDialog, DatePickerDialog

---
+++ PickerDialog/app/src/main/java/com/example/pickerdialog/DatePickerFragment.kt
@@ -0,0 +1,26 @@
+package com.example.pickerdialog
+
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.widget.DatePicker
+import android.widget.TextView
+import androidx.fragment.app.DialogFragment
+import java.util.*
+
+class DatePickerFragment : DialogFragment(), DatePickerDialog.OnDateSetListener {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        return DatePickerDialog(requireContext(), this, year, month, day)
+    }
+
+    override fun onDateSet(p0: DatePicker?, year: Int, month: Int, day: Int) {
+        (activity as MainActivity).findViewById<TextView>(R.id.date_picker_text).text =
+            getString(R.string.date_text, year, month, day)
+    }
+}
---
+++ PickerDialog/app/src/main/java/com/example/pickerdialog/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.pickerdialog
+
+import android.app.TimePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TimePicker
+import androidx.fragment.app.FragmentActivity
+import com.example.pickerdialog.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun showTimePickerDialog(v: View) {
+        TimePickerFragment().show(supportFragmentManager, "timePicker")
+    }
+
+    fun showDatePickerDialog(v:View){
+        DatePickerFragment().show(supportFragmentManager, "datePicker")
+    }
+}
