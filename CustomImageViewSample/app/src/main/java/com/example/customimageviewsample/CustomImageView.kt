56f7278fa51f09ad34a4ed8725c0bb7140ad17f5
mtnmr <mei.rila1013@gmail.com>
2022-07-28 17:28:33 +0900

    imageview pinchIn/Out

---
+++ CustomImageViewSample/app/src/main/java/com/example/customimageviewsample/CustomImageView.kt
@@ -0,0 +1,96 @@
+package com.example.customimageviewsample
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Matrix
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
+import android.view.ScaleGestureDetector
+import androidx.appcompat.widget.AppCompatImageView
+import kotlin.math.max
+import kotlin.math.min
+
+class CustomImageView: AppCompatImageView {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    //公式サンプル
+
+    private var mScaleFactor = 1f
+
+    private val scaleListener = object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+
+        override fun onScale(detector: ScaleGestureDetector): Boolean {
+            mScaleFactor *= detector.scaleFactor
+
+            // Don't let the object get too small or too large.
+            mScaleFactor = max(0.1f, min(mScaleFactor, 5.0f))
+
+            scaleX = mScaleFactor
+            scaleY = mScaleFactor
+
+            invalidate()
+            return true
+        }
+    }
+
+//    override fun onDraw(canvas: Canvas?) {
+//        super.onDraw(canvas)
+//
+//        canvas?.apply {
+//            save()
+//            scale(mScaleFactor, mScaleFactor)
+//            // onDraw() code goes here
+//            restore()
+//        }
+//    }
+
+
+
+/*
+    private var bitmap: Bitmap? = null
+    private val bitmapMatrix = Matrix()
+    private val paint = Paint()
+
+
+    private val scaleListener = object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+
+        private var scale = 1f
+        override fun onScaleBegin(detector: ScaleGestureDetector?): Boolean {
+            scale = 1f
+            return super.onScaleBegin(detector)
+        }
+
+        override fun onScale(detector: ScaleGestureDetector): Boolean {
+            val deltaScale = detector.scaleFactor / scale
+            bitmapMatrix.postScale(deltaScale, deltaScale, detector.focusX, detector.focusY)
+            scale = detector.scaleFactor
+            invalidate()
+            return super.onScale(detector)
+        }
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        bitmap?.let { bitmap ->
+            canvas.save()
+            canvas.drawBitmap(bitmap, bitmapMatrix, paint)
+            canvas.restore()
+        }
+    }
+
+ */
+
+    private val mScaleDetector = ScaleGestureDetector(context, scaleListener)
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(ev: MotionEvent): Boolean {
+        mScaleDetector.onTouchEvent(ev)
+        Log.d("ImageView", "pinchIn/Out")
+        return true
+    }
+}
---
+++ CustomImageViewSample/app/src/main/java/com/example/customimageviewsample/CustomView.kt
@@ -0,0 +1,158 @@
+package com.example.customimageviewsample
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Matrix
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.ScaleGestureDetector
+import android.view.View
+import androidx.appcompat.widget.AppCompatImageView
+
+//自分で描写するView、上手くピンチイン・アウトできた
+//https://qiita.com/vram/items/2496a8cd51cc32ded877
+/*
+class CustomView(context: Context, attrs: AttributeSet?) : View(context, attrs) {
+    var paint: Paint = Paint()
+    var x1 : Float = 0F
+    var y1 : Float = 0F
+    var x0 : Float = 0F
+    var y0 : Float = 0F
+    var scale : Float = 1F
+    override fun onDraw(canvas: Canvas) {
+        canvas.drawColor(Color.argb(255, 0, 127, 0))
+        x0  = (width / 2F)
+        y0  = (height /2F)
+        paint.strokeWidth = 15F
+        paint.color = Color.argb(255, 0, 255, 120)
+        canvas.drawLine(xx(0F),yy(-100F),xx(100F),yy(100F), paint)
+        canvas.drawLine(xx(-100F),yy(100F), xx(100F), yy(100F), paint)
+        canvas.drawLine(xx(-100F),yy(100F),xx(0F),yy(-100F), paint)
+        paint.color = Color.argb(255, 255, 255, 68)
+        paint.strokeWidth = 30F
+        paint.isAntiAlias = true
+        paint.style = Paint.Style.STROKE
+        canvas.drawCircle(xx(0F),yy(0F) , 10F, paint)
+    }
+    fun xx(x : Float) : Float{
+        return x0 + x * scale + x1
+    }
+    fun yy(y : Float) : Float{
+        return y0 + y * scale + y1
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(ev: MotionEvent): Boolean {
+        mScaleDetector.onTouchEvent(ev)
+        mGestureDetector.onTouchEvent(ev)
+        return true
+    }
+
+    private val scaleListener = object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+
+        override fun onScale(detector: ScaleGestureDetector): Boolean {
+            scale *= detector.scaleFactor
+            invalidate()
+            return true
+        }
+    }
+
+    private val gestureListener = object : GestureDetector.SimpleOnGestureListener() {
+        override fun onScroll(
+            e1: MotionEvent,
+            e2: MotionEvent,
+            distanceX: Float,
+            distanceY: Float
+        ): Boolean {
+            x1 -= distanceX
+            y1 -= distanceY
+            invalidate()
+            return true
+        }
+    }
+    private val mScaleDetector = ScaleGestureDetector(context, scaleListener)
+    private val mGestureDetector = GestureDetector(context,gestureListener)
+}
+
+ */
+
+
+class CustomView: AppCompatImageView {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+
+
+    private var bitmapMatrix = Matrix()
+    private val SCALE_MAX = 3.0f
+    private val SCALE_MIN = 0.5f
+    private val PINCH_SENSITIVITY = 5.0f
+
+    private val scaleListener = object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+
+        private var focusX : Float ?= null
+        private var focusY : Float ?= null
+        var scaleFactor = 1.0f
+
+        override fun onScaleBegin(detector: ScaleGestureDetector?): Boolean {
+            focusX = detector?.focusX
+            focusY = detector?.focusY
+            return super.onScaleBegin(detector)
+        }
+
+        override fun onScale(detector: ScaleGestureDetector): Boolean {
+
+            val previousScale: Float = getMatrixValue(Matrix.MSCALE_Y)
+
+            scaleFactor = if (detector.scaleFactor >= 1.0f) {
+                1 + (detector.scaleFactor - 1) / (previousScale * PINCH_SENSITIVITY)
+            } else {
+                1 - (1 - detector.scaleFactor) / (previousScale * PINCH_SENSITIVITY)
+            }
+
+            val scale = scaleFactor * previousScale
+
+            if (scale < SCALE_MIN) {
+                return false
+            }
+
+            if (scale > SCALE_MAX) {
+                return false
+            }
+
+            matrix.postScale(scaleFactor, scaleFactor, focusX!!, focusY!!)
+
+            invalidate()
+            return super.onScale(detector)
+        }
+
+        override fun onScaleEnd(detector: ScaleGestureDetector?) {
+            super.onScaleEnd(detector)
+        }
+    }
+
+    private fun getMatrixValue(index: Int): Float {
+        if (bitmapMatrix == null) {
+            bitmapMatrix = imageMatrix
+        }
+        val values = FloatArray(9)
+        bitmapMatrix.getValues(values)
+        return values[index]
+    }
+
+    private val mScaleDetector = ScaleGestureDetector(context, scaleListener)
+
+    override fun onTouchEvent(ev: MotionEvent): Boolean {
+        imageMatrix = bitmapMatrix
+        mScaleDetector.onTouchEvent(ev)
+        Log.d("ImageView", "pinchIn/Out")
+        return true
+    }
+
+}
